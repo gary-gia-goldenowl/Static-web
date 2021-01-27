b1c9b85bf2fcde952557e5d86301903ea74fb9d9
gary-gia-goldenowl <75287961+gary-gia-goldenowl@users.noreply.github.com>
2021-01-27 11:04:49 +0700

    Update detail.js

--- js/detail.js
+++ js/detail.js
@@ -26,34 +26,6 @@ function ShowImg(){
     thumbnail[2].addEventListener("click" , function(){
         MainImg.src = thumbnailImg[2].src
     })
-document.addEventListener("DOMContentLoaded", function(){   
-    var MainImg = document.getElementById("MainImg")
-    var thumbnail = document.getElementsByClassName("product-thumbnail")
-    var thumbnailImg = document.getElementsByClassName("product-thumbnail-img")
-
-
-    for (var i=0;i<thumbnail.length;i++){
-        thumbnail[i].addEventListener("click",ShowImg)
-    }
-
-    MainImg.addEventListener("mousemove" , function(event){
-        ZoomImg(event);
-    })
-
-    MainImg.addEventListener("mouseleave" , ZoomOut)
-
-
-function ShowImg(){
- 
-    thumbnail[0].addEventListener("click" , function(){
-        MainImg.src = thumbnailImg[0].src
-    })
-    thumbnail[1].addEventListener("click" , function(){
-        MainImg.src = thumbnailImg[1].src
-    })
-    thumbnail[2].addEventListener("click" , function(){
-        MainImg.src = thumbnailImg[2].src
-    })
 
 }
 
@@ -64,34 +36,13 @@ function ZoomImg(event){
     clientY = event.clientY - MainImg.offsetTop
     Width =  MainImg.offsetWidth
     Height =  MainImg.offsetHeight
-    clientX = clientX / Width * 5
-    clientY = clientY / Height * 15
+    clientX = clientX / Width * 55
+    clientY = clientY / Height * 40
     MainImg.style.transform = "translate(-"+clientX+"%,-"+clientY+"%) scale(1.5)"
 
 }   
 function ZoomOut(){
-    MainImg.style.transform = "translate(0%,0%) scale(1)"
-}
-/*------*/
-});
-
-
-}
-
-
-function ZoomImg(event){
-
-    clientX = event.clientX - MainImg.offsetLeft
-    clientY = event.clientY - MainImg.offsetTop
-    Width =  MainImg.offsetWidth
-    Height =  MainImg.offsetHeight
-    clientX = clientX / Width * 30
-    clientY = clientY / Height * 80
-    MainImg.style.transform = "translate(-"+clientX+"%,-"+clientY+"%) scale(2)"
-    
-}
-function ZoomOut(){
-    MainImg.style.transform = "translate(0%,0%) scale(1)"
+    MainImg.style.transform = "translate(-50%,-20%) scale(1)"
 }
 /*------*/
 });
