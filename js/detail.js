59a1bc8c73bbdab8172a3e15513c0362bb4becde
gary-gia-goldenowl <75287961+gary-gia-goldenowl@users.noreply.github.com>
2021-01-27 10:46:37 +0700

    Update detail.js

--- js/detail.js
+++ js/detail.js
@@ -26,6 +26,55 @@ function ShowImg(){
     thumbnail[2].addEventListener("click" , function(){
         MainImg.src = thumbnailImg[2].src
     })
+document.addEventListener("DOMContentLoaded", function(){   
+    var MainImg = document.getElementById("MainImg")
+    var thumbnail = document.getElementsByClassName("product-thumbnail")
+    var thumbnailImg = document.getElementsByClassName("product-thumbnail-img")
+
+
+    for (var i=0;i<thumbnail.length;i++){
+        thumbnail[i].addEventListener("click",ShowImg)
+    }
+
+    MainImg.addEventListener("mousemove" , function(event){
+        ZoomImg(event);
+    })
+
+    MainImg.addEventListener("mouseleave" , ZoomOut)
+
+
+function ShowImg(){
+ 
+    thumbnail[0].addEventListener("click" , function(){
+        MainImg.src = thumbnailImg[0].src
+    })
+    thumbnail[1].addEventListener("click" , function(){
+        MainImg.src = thumbnailImg[1].src
+    })
+    thumbnail[2].addEventListener("click" , function(){
+        MainImg.src = thumbnailImg[2].src
+    })
+
+}
+
+
+function ZoomImg(event){
+
+    clientX = event.clientX - MainImg.offsetLeft 
+    clientY = event.clientY - MainImg.offsetTop
+    Width =  MainImg.offsetWidth
+    Height =  MainImg.offsetHeight
+    clientX = clientX / Width * 5
+    clientY = clientY / Height * 15
+    MainImg.style.transform = "translate(-"+clientX+"%,-"+clientY+"%) scale(1.5)"
+
+}   
+function ZoomOut(){
+    MainImg.style.transform = "translate(0%,0%) scale(1)"
+}
+/*------*/
+});
+
 
 }
 
